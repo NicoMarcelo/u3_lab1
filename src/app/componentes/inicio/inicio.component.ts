408b1ba73677560f442441f46ef1a0fc70b2654c
Nicolás Heredia Oros <nicolas.heredia.oros@cftmail.cl>
2022-11-18 11:36:12 -0300

    subida del proyecto componente/servicios

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+import { FormularioService } from 'src/app/servicios/formulario.service';
 
 @Component({
   selector: 'app-inicio',
@@ -11,11 +12,13 @@ export class InicioComponent implements OnInit {
 
   formulario:any;
 
-  constructor(private fb:FormBuilder) { }
+  constructor(
+    private formularioSrv:FormularioService,
+    private fb:FormBuilder) { }
 
   ngOnInit(): void {
     this.formulario = this.fb.group({
-      nombre:['',[Validators.required, Validators.minLength(4)]],
+      nombre:['',[Validators.required, Validators.minLength(3)]],
       apellido:['',[Validators.required, Validators.minLength(4)]],
       edad:['',[Validators.required, Validators.min(5)]],
       email:['',Validators.required]
@@ -28,6 +31,7 @@ export class InicioComponent implements OnInit {
   }
 
   botonEnviar(){
-    console.log(this.formulario.value)
+    this.formularioSrv.obtener_datos(this.formularioReactivo)
+    //console.log(this.formulario.value);
   }
 }
